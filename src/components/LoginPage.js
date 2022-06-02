3348fd8c8f590cbd39fecde76adfd2f082136202
Wizard-F <forrest.futao.wei@gmail.com>
2022-06-02 22:15:03 +0800

    Refactor LoginPage with MUI

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,8 @@
 import { useState, useContext } from "react";
 import axios from "axios";
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 import UserContext from "./UserContext";
 import { Navigate } from "react-router-dom";
@@ -31,25 +34,53 @@ export default function LoginPage() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Box>
+        <TextField
+          label='Email'
           value={email}
           onChange={event => setEmail(event.target.value)}
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Box>
+      <Box>
+        <TextField
+          label="Password"
           type="password"
           value={password}
           onChange={event => setPassword(event.target.value)}
         />
-      </label>
-      <input type="submit" value="Login" />
-    </form>
+      </Box>
+      <Box>
+        <Button variant='contained' type="submit">
+          Login
+        </Button>
+      </Box>
+    </Box>
+    // <form onSubmit={handleSubmit}>
+    //   <label>
+    //     Email:
+    //     <input
+    //       type="email"
+    //       value={email}
+    //       onChange={event => setEmail(event.target.value)}
+    //     />
+    //   </label>
+    //   <label>
+    //     Password:
+    //     <input
+    //       type="password"
+    //       value={password}
+    //       onChange={event => setPassword(event.target.value)}
+    //     />
+    //   </label>
+    //   <input type="submit" value="Login" />
+    // </form>
   );
 };
 
